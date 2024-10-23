caaedc1f3404437f7cf41b935dc1f359cd400757
Victoria Breen <victoriabreen8@hotmail.com>
2024-10-23 12:11:02 -0230

    Setting up author

--- author.java
+++ author.java
@@ -1,4 +1,28 @@
 // check for packages
+import java.util.ArrayList;
+import java.util.List;
+
 public class author {
-    
+    private String name;
+    private String dateOfBirth;
+    private List <LibraryItem> writtenItems;  // anything the author has worked on 
+
+    // constructors
+    public author (String name, String dateOfBirth){
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.writtenItems = new ArrayList<>();
+    }
+
+    public void addWrittenItem(LibraryItem item) {
+        writtenItems.add(item); // adding the author work to their list
+
+    }
+
+    public void removeWrittenItem(LibraryItem item) {
+        writtenItems.remove(item); // removing the author work from the list 
+    }
+
+    // getters and setters
+
 }
